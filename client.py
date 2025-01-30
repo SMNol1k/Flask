183bbd13f5280d014959f7ee4bd1595970504e95
Test <roqueknight123456@gmail.com>
2025-01-30 13:46:19 +0300

    Final commit

--- client.py
+++ client.py
@@ -1,21 +1,20 @@
 import requests
 
-response = requests.post(
-    'http://127.0.0.1:5000/api',
-    json={'title': 'Объявление', 'content': 'Ваше новое объявление'},
+# response = requests.post(
+#     'http://127.0.0.1:5000/api',
+#     json={'id':'5', 'title': 'Test Ad', 'content': 'This is a test advertisement.', 'owner': 'user2'},
+# )
+# print(response.status_code)
+# print(response.json())
+
+response = requests.delete(
+    "http://127.0.0.1:5000/api/5",
 )
 print(response.status_code)
 print(response.text)
 
-
-# response = requests.delete(
-#     "http://127.0.0.1:5000/api/1/",
-# )
-# print(response.status_code)
-# print(response.text)
-#
 # response = requests.get(
-#     "http://127.0.0.1:5000/api/1",
+#     "http://127.0.0.1:5000/api/5",
 # )
 # print(response.status_code)
 # print(response.text)
